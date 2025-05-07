67ea3a550ecaaf4b9c21be32ded870aba6bb26a9
Stas <smoshell9@gmail.com>
2025-05-07 10:13:16 +0300

    update

--- backend-django/shop/serializers.py
+++ backend-django/shop/serializers.py
@@ -3,7 +3,6 @@ from .models import *
 
 
 class CourseSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Course
         fields = ['title', 'price', 'students_qty', 'category']
