12bf0e8db8bb3d556950308d6e822963f96ee26b
Stas <smoshell9@gmail.com>
2025-05-07 10:21:29 +0300

    Add navbar component with initial structure

---
+++ frontend-react/react_root/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            
+
+        </div>
+    </nav>
+  )
+}
+
+export default navbar
